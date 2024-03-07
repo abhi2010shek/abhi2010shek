61f32984ef3375000b5e5a7a0b82c4cb9b34d797
Abhishek Kumar Raghuvanshi <98256155+abhi2010shek@users.noreply.github.com>
2024-03-07 10:36:16 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Longest repeating and non-overlapping substring/longest-repeating-and-nonoverlapping-substring.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string longestSubstring(string s, int n) {
+        // code here
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int max_length=0;
+        int last=0;
+        for(int i=1;i<=n;i++){
+            for(int j=i+1;j<=n;j++){
+                if(s[i-1]==s[j-1] && dp[i-1][j-1]<(j-i)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    if(max_length<dp[i][j]){
+                        max_length=dp[i][j];
+                        last=i-1;
+                    }
+                }
+            }
+        }
+        if(max_length==0)
+        return "-1";
+        return s.substr(last-max_length+1,max_length);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        string S;
+
+        cin >> N;
+        cin >> S;
+
+        Solution ob;
+        cout << ob.longestSubstring(S, N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
