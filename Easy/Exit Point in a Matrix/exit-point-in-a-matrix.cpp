84b73ad2e611db8688fbff3ffb7ffeef9e1ad8c0
Abhishek Kumar Raghuvanshi <98256155+abhi2010shek@users.noreply.github.com>
2024-04-26 22:01:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Exit Point in a Matrix/exit-point-in-a-matrix.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> FindExitPoint(int n, int m, vector<vector<int>>& matrix) {
+        // Code here
+        int i = 0, j = 0;
+        int dir = 1;
+
+        /*Now we loop till we don't find the boundary */
+
+        while (i < n and j < m and i >= 0 and j >= 0) {
+
+            /*Changes the directions if 1 encountered and resets value to 0*/
+            if (matrix[i][j] == 1) {
+                matrix[i][j] = 0;
+                switch (dir) {
+                    case 1:
+                        dir = 2;
+                        break;
+                    case 2:
+                        dir = 3;
+                        break;
+                    case 3:
+                        dir = 4;
+                        break;
+                    case 4:
+                        dir = 1;
+                        break;
+                }
+            }
+
+            /* moves along  based on the direction*/
+            switch (dir) {
+                case 1:
+                    j++;
+                    break;
+                case 2:
+                    i++;
+                    break;
+                case 3:
+                    j--;
+                    break;
+                case 4:
+                    i--;
+                    break;
+            }
+        }
+
+        /* to compensate for the last move */
+        switch (dir) {
+            case 1:
+                j--;
+                break;
+            case 2:
+                i--;
+                break;
+            case 3:
+                j++;
+                break;
+            case 4:
+                i++;
+                break;
+        }
+
+        return {i, j};
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> matrix(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> matrix[i][j];
+        Solution obj;
+        vector<int> ans = obj.FindExitPoint(n, m, matrix);
+        for (auto i : ans)
+            cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
