0b679e3995aad7745fc72424fe567d84fa753eef
Abhishek Kumar Raghuvanshi <98256155+abhi2010shek@users.noreply.github.com>
2024-06-11 07:48:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Tip Calculator/maximum-tip-calculator.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &a, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &a[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &a) {
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    using ll = long long;
+    long long maxTip(int n, int x, int y, vector<int> &arr, vector<int> &brr) {
+        ll ans = 0, sum = 0;
+        int my = n - min(x, n), c = 0;
+        priority_queue<int> pq;
+        for(auto a : arr)
+        sum += a;
+        for(int i = 0; i < n; i++){
+            pq.push(brr[i] - arr[i]);
+        }
+        while(c < my){
+            sum += pq.top();
+            pq.pop();
+            c++;
+        }
+        while(c <= y){
+            ans = max(ans, sum);
+            sum += pq.top();
+            pq.pop();
+            c++;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        int y;
+        scanf("%d", &y);
+
+        vector<int> arr(n);
+        Array::input(arr, n);
+
+        vector<int> brr(n);
+        Array::input(brr, n);
+
+        Solution obj;
+        long long res = obj.maxTip(n, x, y, arr, brr);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
