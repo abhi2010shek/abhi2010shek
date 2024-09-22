56ebaa2d45fc2b28ce950f9bb541c8bdea532c76
Abhishek Kumar Raghuvanshi <98256155+abhi2010shek@users.noreply.github.com>
2024-09-22 15:56:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int lps(string str) {
+        // Your code goes here
+        int n =str.length();
+        int i=1,j=0;
+        vector<int>match(n,0);
+        while(i<n)
+        {
+            if(str[i]==str[j]) match[i++]=++j;
+            else
+           {   if(j>0)j=match[j-1];
+           else i++;
+               
+           }
+        }
+        return match[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+
+        Solution ob;
+
+        cout << ob.lps(str) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
