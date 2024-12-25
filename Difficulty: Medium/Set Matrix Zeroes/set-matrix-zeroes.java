4eaf4419a54b9884e3ef71d905a893b932d74055
Abhishek Kumar Raghuvanshi <98256155+abhi2010shek@users.noreply.github.com>
2024-12-25 10:53:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int arr[][] = new int[n][m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            new Solution().setMatrixZeroes(arr);
+            for (int[] row : arr) {
+                for (int val : row) {
+                    System.out.print(val + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//Back-end complete function Template for Java
+class Solution {
+    public void setMatrixZeroes(int[][] mat) {
+        int i, j;
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        int[] row = new int[n];
+        int[] col = new int[m];
+        
+        for(i=0; i < n; i++){
+            for(j=0; j < m; j++){
+                if(mat[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        
+        for(i=0; i < n; i++){
+            if(row[i] == 1){
+                for(j=0; j < m; j++){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        
+        for(j=0; j < m; j++){
+            if(col[j] == 1){
+                for(i=0; i < n; i++){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        
+        return;
+    }
+}
